842234c9c5f16d5207de7d76037d97aeffd2b69d
mchydhamlna <144500877+nalesyn@users.noreply.github.com>
2024-01-23 00:11:01 +0700

    Update program.cpp

--- Tugas 1/program/program.cpp
+++ Tugas 1/program/program.cpp
@@ -9,8 +9,8 @@ int main()
 	
 	cout <<endl;
 	cout <<"----------- Warnet KKB Ikopin ---------- \n";
-    cout <<"------------------------------------------ \n";
-    cout << endl;
+        cout <<"------------------------------------------ \n";
+        cout << endl;
 	
 	cout<<"=========================================="<<endl;
 	cout<<"No Komputer : ";cin>>nocom;
